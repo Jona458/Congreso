6f1167d021393bdb72bfc73cf6167f7832c917fb
Erick Olguin <mariobroserick@hotmail.com>
2023-09-24 14:52:59 -0600

    Componentes

---
+++ src/app/talleristas/talleristas.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-talleristas',
+  templateUrl: './talleristas.component.html',
+  styleUrls: ['./talleristas.component.scss'],
+})
+export class TalleristasComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
